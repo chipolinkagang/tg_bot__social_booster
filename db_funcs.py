1b52348c28445d98ec8f676993b0cc27e05d2d8f
chipolinkagang <kirillgzhukov@gmail.com>
2022-08-13 19:56:57 +0300

    .

--- db_funcs.py
+++ db_funcs.py
@@ -15,7 +15,8 @@ users = sa.Table('users', metadata,
                  sa.Column('id', sa.Integer, primary_key=True),
                  sa.Column('name', sa.String(255)),
                  sa.Column('tg_id', sa.String(255)),
-                 sa.Column('balance', sa.Integer))
+                 sa.Column('balance', sa.Integer),
+                 sa.Column('now_task', sa.String))
 
 orders = sa.Table('orders', metadata,
                   sa.Column('id', sa.Integer, primary_key=True),
@@ -25,6 +26,12 @@ orders = sa.Table('orders', metadata,
                   sa.Column('value', sa.Integer),
                   sa.Column('date', sa.String(255)))
 
+personal_prices = sa.Table('personal_prices', metadata,
+                           sa.Column('id', sa.Integer, primary_key=True),
+                           sa.Column('tg_id', sa.String(255)),
+                           sa.Column('type', sa.Integer),
+                           sa.Column('price', sa.String))
+
 list_o = sa.Table('list_o', metadata,
                   sa.Column('id', sa.Integer, primary_key=True),
                   sa.Column('type', sa.String(255)),
@@ -33,33 +40,44 @@ list_o = sa.Table('list_o', metadata,
 
 async def create_table(engine):
     async with engine.acquire() as conn:
+        await conn.execute('DROP TABLE IF EXISTS personal_prices')
+        await conn.execute('''CREATE TABLE personal_prices (
+                                                  id serial PRIMARY KEY,
+                                                  tg_id varchar(255),
+                                                  type integer,
+                                                  price varchar(255))''')
+
         await conn.execute('DROP TABLE IF EXISTS users')
         await conn.execute('''CREATE TABLE users (
                                           id serial PRIMARY KEY,
                                           name varchar(255),
                                           tg_id varchar(255),
-                                          balance integer)''')
+                                          balance integer,
+                                          now_task varchar(255))''')
 
-        await conn.execute('DROP TABLE IF EXISTS orders')
-        await conn.execute('''CREATE TABLE orders (
-                                          id serial PRIMARY KEY,
-                                          tg_id varchar(255),
-                                          url varchar(255),
-                                          type_id integer,
-                                          value integer,
-                                          date varchar(255))''')
+        # await conn.execute('DROP TABLE IF EXISTS orders')
+        # await conn.execute('''CREATE TABLE orders (
+        #                                   id serial PRIMARY KEY,
+        #                                   tg_id varchar(255),
+        #                                   url varchar(255),
+        #                                   type_id integer,
+        #                                   value integer,
+        #                                   date varchar(255))''')
+        #
+        # await conn.execute('DROP TABLE IF EXISTS list_o')
+        # await conn.execute('''CREATE TABLE list_o (
+        #                                   id serial PRIMARY KEY,
+        #                                   type varchar(255),
+        #                                   price integer)''')
 
-        await conn.execute('DROP TABLE IF EXISTS list_o')
-        await conn.execute('''CREATE TABLE list_o (
-                                          id serial PRIMARY KEY,
-                                          type varchar(255),
-                                          price integer)''')
 
 # проверка на регистрацию
 async def reg_check(engine, inp_tg_id: str) -> bool:
     async with engine.acquire() as conn:
         async for row in conn.execute(users.select().where(users.c.tg_id == inp_tg_id)):
             return True
+
+
 # Добавление нового пользователя в users
 async def registration(engine, person: dict) -> str:
     async with engine.acquire() as conn:
@@ -74,6 +92,7 @@ async def registration(engine, person: dict) -> str:
 async def reg_type(engine, list_order: dict):
     async with engine.acquire() as conn:
         await conn.execute(users.insert().values(type=list_order['type'], price=list_order['price']))
+
 
 # Добавление нового заказа в orders (orders.type_id = list.id) {"tg_id": , "type_id": , "url": , "value": }
 async def new_order(engine, order: dict) -> str:
@@ -95,14 +114,17 @@ async def add_balance(engine, inp_tg_id: str, val: int) -> int:
     async with engine.acquire() as conn:
         async for row in conn.execute(users.select().where(users.c.tg_id == inp_tg_id)):
             now_balance = row.balance
-            await conn.execute(sa.update(users).values({"balance": now_balance + val}).where(users.c.tg_id == inp_tg_id))
+            await conn.execute(
+                sa.update(users).values({"balance": now_balance + val}).where(users.c.tg_id == inp_tg_id))
             return now_balance + val
+
 
 # получить текущий баланс по uid
 async def get_balance(engine, int_tg_id: str):
     async with engine.acquire() as conn:
         async for row in conn.execute(users.select().where(users.c.tg_id == int_tg_id)):
             return row.balance
+
 
 # получить текущую цену для конкретного id товара
 async def get_price(engine, type_id: int):
@@ -128,6 +150,7 @@ async def get_report(engine, inp_tg_id: str, inp_type_id: int) -> List[dict]:
             print(res)
             return res
 
+
 # Вывод всех значений таблицы
 async def get_all(engine):
     async with engine.acquire() as conn:
@@ -135,6 +158,18 @@ async def get_all(engine):
         async for row in conn.execute(users.select()):
             res.append({"id": row.id, "name": row.name, "tg_id": row.tg_id, "balance": row.balance})
         return res
+
+
+async def set_now_task(engine, inp_tg_id: str, inp_type_id: str):
+    async with engine.acquire() as conn:
+        await conn.execute(sa.update(users).values({"now_task": inp_type_id}).where(users.c.tg_id == inp_tg_id))
+
+
+async def get_now_task(engine, inp_tg_id: str):
+    async with engine.acquire() as conn:
+        async for row in conn.execute(users.select().where(users.c.tg_id == inp_tg_id)):
+            return row.now_task
+
 
 async def go():
     async with create_engine(user='postgres',
@@ -166,7 +201,6 @@ async def go():
         # await new_order(engine,
         #                 {'tg_id': '862989874', 'url': 'vk.com/chipoli/wafdgfll_', 'type_id': 2, 'value': 2000})
 
-
         # await add_balance(engine, '8573275', -15)
         # await get_report(engine, '8573275', 1)
         # await set(engine, {"id": "2", "first_name": "rer", "last_name": "fef"})
@@ -182,6 +216,7 @@ async def go():
         #     async for row in conn.execute(peop.select()):
         #         print(row.id, row.first_name, row.last_name)
         print(await get_all(engine))
+        # print(await set_now_task(engine, "862989874", "2"))
 
 
 loop = asyncio.get_event_loop()
--- tg_api.py
+++ tg_api.py
@@ -17,7 +17,7 @@ from aiopg.sa import create_engine
 
 import markups as nav
 
-bot = Bot(token='5489920134:AAGkMf2P9b22kpAGkRWLV6QAd6b6jyIN85k')
+bot = Bot(token='5461590476:AAH8XC2qfAHQh0yc4KDnSkcRwfpVPDSY6Eo')
 dp = Dispatcher(bot)
 
 
@@ -37,17 +37,17 @@ async def send_welcome(message: types.Message):
             await message.reply("Вы уже зарегистрированы. Переходим к меню", reply_markup=nav.mainMenu)
 
 
-class Task_now:
-    task_id = 0  # 1 - лайки, 2 - просмотры
-
-    def choose_task(self, _task: int):
-        self.task_id = _task
-
-    def check_task(self):
-        return self.task_id
-
-
-task = Task_now()
+# class Task_now:
+#     task_id = 0  # 1 - лайки, 2 - просмотры
+#
+#     def choose_task(self, _task: int):
+#         self.task_id = _task
+#
+#     def check_task(self):
+#         return self.task_id
+#
+#
+# task = Task_now()
 
 
 @dp.message_handler()
@@ -57,15 +57,18 @@ async def echo_message(msg: types.Message):
                              host='127.0.0.1',
                              password='123456') as engine:
         if msg.text == "⬅️ Главное меню":
-            task.choose_task(0)
+            # task.choose_task(0)
+            await db_funcs.set_now_task(engine, str(msg.from_user.id), "0")
             await bot.send_message(msg.from_user.id, "Вернулись в главное меню.", reply_markup=nav.mainMenu)
 
         # for main menu:
         elif msg.text == "🆕 ЗАКАЗ":
-            task.choose_task(0)
+            # task.choose_task(0)
+            await db_funcs.set_now_task(engine, str(msg.from_user.id), "0")
             await bot.send_message(msg.from_user.id, "Выберите тип продвижения.", reply_markup=nav.orderMenu)
         elif msg.text == "ℹ️ Профиль":
-            task.choose_task(0)
+            # task.choose_task(0)
+            await db_funcs.set_now_task(engine, str(msg.from_user.id), "0")
             uid = str(msg.from_user.id)
             balance = await db_funcs.get_balance(engine, str(msg.from_user.id))
             ans = "Информация:\nTelegram id: " + uid + "\nБаланс: " + str(
@@ -75,7 +78,8 @@ async def echo_message(msg: types.Message):
             uid = str(msg.from_user.id)
             get_like = await db_funcs.get_report(engine, uid, 1)
             get_view = await db_funcs.get_report(engine, uid, 2)
-            task.choose_task(0)
+            # task.choose_task(0)
+            await db_funcs.set_now_task(engine, str(msg.from_user.id), "0")
             await bot.send_message(msg.from_user.id,
                                    "Статистика:\n\nЛайки:\nЗаказов: " + str(get_like['res_orders']) + " штук\nВсего: " +
                                    str(get_like['res_sum']) + " лайков\n\nПросмотры:\nЗаказов: " + str(
@@ -83,26 +87,34 @@ async def echo_message(msg: types.Message):
                                    str(get_view['res_sum']) + " просмотров")
         # for order menu:
         elif msg.text == "🔝 Лайки":
-            task.choose_task(1)
+            # task.choose_task(1)
+            await db_funcs.set_now_task(engine, str(msg.from_user.id), "1")
             await bot.send_message(msg.from_user.id,
                                    "Введите ссылку и количество лайков через пробел.\nЦена 60 рублей за 1000 лайков\nПример:\nhttps://vk.com/wall-22822305_1307837 110",
                                    reply_markup=nav.orderMenu)
         elif msg.text == "👁‍🗨 Просмотры":
-            task.choose_task(2)
+            # task.choose_task(2)
+            await db_funcs.set_now_task(engine, str(msg.from_user.id), "2")
             await bot.send_message(msg.from_user.id,
                                    "Введите ссылку и количество просмотров через пробел.\nЦена 10 рублей за 1000 просмотровl\nПример:\nhttps://vk.com/wall-22822305_1307837 3200",
                                    reply_markup=nav.orderMenu)
+        elif msg.text[0:10] == "addbalance":
+            add_balance = msg.text.split()
+            await db_funcs.add_balance(engine, add_balance[1], int(add_balance[2]))
+            await bot.send_message(msg.from_user.id,
+                                   "Баланс обновлен: " + add_balance[1] + " tg_id, на " + add_balance[2] + "рублей")
         elif msg.text[0:5] == "https":
             order_list = msg.text.split()
             if len(order_list) == 2 and int(order_list[1]) > 0:
                 uid = str(msg.from_user.id)
-                if task.check_task() == 1:
+                # if task.check_task() == 1:
+                if await db_funcs.get_now_task(engine, str(msg.from_user.id)) == "1":
                     try:
                         sum = math.ceil(int(order_list[1]) / 1000 * 60)
                         if (await db_funcs.get_balance(engine, str(msg.from_user.id)) - sum) > 0:
                             like_api.make_like(str(uid), order_list[0], str(order_list[1]))
                             await bot.send_message(msg.from_user.id, "Задание успешно поставлено")
-                            await db_funcs.new_order(engine, {"tg_id": uid, "type_id": task.check_task(), "url": order_list[0],
+                            await db_funcs.new_order(engine, {"tg_id": uid, "type_id": "1", "url": order_list[0],
                                                       "value": order_list[1]})
                             await db_funcs.add_balance(engine, uid, -sum)
                         else:
@@ -110,20 +122,22 @@ async def echo_message(msg: types.Message):
 
                     except Exception as ex:
                         await bot.send_message(msg.from_user.id, "Ошибка:" + str(ex))
-                if task.check_task() == 2:
+                # if task.check_task() == 2:
+                if await db_funcs.get_now_task(engine, str(msg.from_user.id)) == "2":
                     try:
                         sum = math.ceil(int(order_list[1]) / 1000 * 10)
                         if (await db_funcs.get_balance(engine, str(msg.from_user.id)) - sum) > 0:
                             view_api.make_view(order_list[0], str(order_list[1]))
                             await bot.send_message(msg.from_user.id, "Задание успешно поставлено")
-                            await db_funcs.new_order(engine, {"tg_id": uid, "type_id": task.check_task(), "url": order_list[0],
+                            await db_funcs.new_order(engine, {"tg_id": uid, "type_id": "2", "url": order_list[0],
                                                           "value": order_list[1]})
                             await db_funcs.add_balance(engine, uid, -sum)
                         else:
                             await bot.send_message(msg.from_user.id, "Недостаточно средств.")
                     except Exception as ex:
                         await bot.send_message(msg.from_user.id, "Ошибка:" + str(ex))
-                if task.check_task() == 0:
+                # if task.check_task() == 0:
+                if await db_funcs.get_now_task(engine, str(msg.from_user.id)) == "0":
                     await bot.send_message(msg.from_user.id, "Не выбран тип накрутки.")
             else:
                 await bot.send_message(msg.from_user.id, "Неверный формат ввода.\nПример верного формата:\nhttps://vk.com/wall-22822305_1307837 3200\n(ссылка_на_пост, 1 пробел, количество)")
