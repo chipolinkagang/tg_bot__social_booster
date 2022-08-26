d74439804fc98534544cb4d39a8b400803b15511
chipolinkagang <kirillgzhukov@gmail.com>
2022-08-27 01:05:38 +0300

    +payments :)

--- config.py
+++
@@ -1,3 +0,0 @@
-chrome_profile = "user-data-dir=C:\\Users\\kiril\\AppData\\Local\\Google\\Chrome Beta\\User Data\\Profile 1"
-chrome_binary_exe_argument = "C:\\Program Files\\Google\\Chrome Beta\\Application\\chrome.exe"
-chrome_driver_argument = "C:\\Users\\kiril\\PycharmProjects\\tg_bot\\chromedriver.exe"
